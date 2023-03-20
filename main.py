90d49ce3937bb86667d497e76db4418accd94b02
Vincent <vincent.qi@hotmail.com>
2023-03-20 11:47:04 -0700

    feat: read/write data to csv

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
 # SPDX-License-Identifier: MIT
-
+import csv
 import time
 import board
 import digitalio
@@ -17,7 +17,8 @@ fan_ctrl = GPIO.PWM(fan_pin, 50)
 fan_ctrl.start(50);
 fan_ctrl.ChangeDutyCycle(0)
 
-def start_record(percent):
+def start_record(speed):
+    percent = (speed / 1500) * 100
     fan_ctrl.ChangeDutyCycle(percent)
     input("Press Enter to start recording...")
     print("Recording...")
@@ -31,13 +32,18 @@ def start_record(percent):
 
     temp_end = max31855.temperature
 
+    with open('trial_{}.csv'.format(trial), 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow([speed, temp_end])
+
+
     print("Recording stopped with final temperature of {}C".format(temp_end))
 
 speed = int(input("Enter a fan speed (500 - 1500)"))
+trial = int(input("Enter a trial number"))
 
 while speed > 500:
-    percent = (speed / 1500) * 100
-    start_record(percent)
+    start_record(speed)
     speed -= 100
 
 print("Finished data collection.")
