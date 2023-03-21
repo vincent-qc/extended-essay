f9489541d3d191b67600d8b658cf665437c0d82b
Vincent Qi <vincent.qi@hotmail.com>
2023-03-21 11:48:59 -0700

    feat: testing and data files

---
+++ testing.py
@@ -0,0 +1,20 @@
+import csv
+import time
+import board
+import digitalio
+import adafruit_max31855
+import RPi.GPIO as GPIO
+
+spi = board.SPI()
+cs = digitalio.DigitalInOut(board.D5)
+
+max31855 = adafruit_max31855.MAX31855(spi, cs)
+fan_pin = 12
+GPIO.setup(fan_pin, GPIO.OUT)
+fan_ctrl = GPIO.PWM(fan_pin, 50)
+fan_ctrl.start(50);
+fan_ctrl.ChangeDutyCycle(0)
+
+while True:
+    print(max31855.temperature)
+    time.sleep(0.1)
